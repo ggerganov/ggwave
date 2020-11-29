5c478fce1206266a3e06f3cf451ac51b0824fb94
Georgi Gerganov <ggerganov@gmail.com>
2020-11-29 15:50:05 +0000

    ggwave-wasm : minor fix

--- examples/ggwave-wasm/main.cpp
+++ examples/ggwave-wasm/main.cpp
@@ -13,7 +13,9 @@ int main(int , char** argv) {
     printf("Build time: %s\n", BUILD_TIMESTAMP);
     printf("Press the Init button to start\n");
 
-    GGWave_setDefaultCaptureDeviceName(argv[1]);
+    if (argv[1]) {
+        GGWave_setDefaultCaptureDeviceName(argv[1]);
+    }
 
     emscripten_set_main_loop(update, 60, 1);
 

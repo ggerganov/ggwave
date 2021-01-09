2e9283b77753c25cc6dd7e267ced28e661e25fc9
Georgi Gerganov <ggerganov@gmail.com>
2021-01-09 15:08:19 +0200

    ggwave-to-file : add python example

---
+++ examples/ggwave-to-file/ggwave-to-file.py
@@ -0,0 +1,22 @@
+import sys
+import requests
+
+def ggwave(message: str, protocolId: int = 1):
+
+    url = 'https://ggwave.ggerganov.com/ggwave-to-file.php'
+
+    params = {
+        'm': message,       # message to encode
+        'p': protocolId,    # transmission protocol to use
+    }
+
+    response = requests.get(url, params=params)
+
+    if response == '':
+        raise SyntaxError('Request failed')
+
+    return response
+
+result = ggwave("Hello world!")
+
+sys.stdout.buffer.write(result.content)

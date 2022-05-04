ecb604c629381161ab030861ec8ed31149562453
Georgi Gerganov <ggerganov@gmail.com>
2022-05-04 20:49:54 +0300

    arduino-tx : ported the mono-tone ggwave protocols

--- examples/r2t2/arduino-tx/arduino-tx.ino
+++ examples/r2t2/arduino-tx/arduino-tx.ino
@@ -1,52 +1,29 @@
-#define kPinLed 13
-#define kPinSpeaker 11
+#include "ggwave.h"
+
+#define kPinLed0 13
+#define kPinSpeaker 10
 
 void setup() {
-    pinMode(kPinLed,     OUTPUT);
+    pinMode(kPinLed0,    OUTPUT);
     pinMode(kPinSpeaker, OUTPUT);
-
-    randomSeed(analogRead(0));
 }
 
-void loop() {
-    if (1) {
-        tone(kPinSpeaker,  1125.00); delay(  64);
-        tone(kPinSpeaker,   937.50); delay(  64);
-        tone(kPinSpeaker,   984.38); delay(  64);
-        tone(kPinSpeaker,  1031.25); delay(  64);
-        tone(kPinSpeaker,  1312.50); delay(  64);
-        tone(kPinSpeaker,  1031.25); delay(  64);
-        tone(kPinSpeaker,  1312.50); delay(  64);
-        tone(kPinSpeaker,  1031.25); delay(  64);
-        tone(kPinSpeaker,  1453.12); delay(  64);
-        tone(kPinSpeaker,  1031.25); delay(  64);
-        tone(kPinSpeaker,   750.00); delay(  64);
-        tone(kPinSpeaker,   843.75); delay(  64);
-        tone(kPinSpeaker,   937.50); delay(  64);
-        tone(kPinSpeaker,   984.38); delay(  64);
-        tone(kPinSpeaker,  1078.12); delay( 128);
-        tone(kPinSpeaker,  1171.88); delay(  64);
-        tone(kPinSpeaker,  1031.25); delay(  64);
-        tone(kPinSpeaker,   937.50); delay(  64);
-        tone(kPinSpeaker,  1078.12); delay(  64);
-        tone(kPinSpeaker,   937.50); delay(  64);
-        tone(kPinSpeaker,  1078.12); delay(  64);
-        tone(kPinSpeaker,   984.38); delay(  64);
-        tone(kPinSpeaker,  1031.25); delay(  64);
-        tone(kPinSpeaker,   843.75); delay(  64);
-        tone(kPinSpeaker,  1078.12); delay(  64);
-        tone(kPinSpeaker,   796.88); delay(  64);
-        tone(kPinSpeaker,   843.75); delay(  64);
-        tone(kPinSpeaker,   796.88); delay(  64);
-        tone(kPinSpeaker,  1453.12); delay(  64);
-        tone(kPinSpeaker,   796.88); delay(  64);
-        tone(kPinSpeaker,  1171.88); delay(  64);
-        tone(kPinSpeaker,  1125.00); delay(  64);
-        tone(kPinSpeaker,  1265.62); delay(  64);
-        tone(kPinSpeaker,  1031.25); delay(  64);
-        tone(kPinSpeaker,  1312.50); delay(  64);
-    }
+char txt[64]; // used for printf
 
-    noTone(kPinSpeaker);
-    delay(3000);
+void loop() {
+    char tx[16];
+    memset(tx, 0, sizeof(tx));
+    strcpy(tx, "Hello World!");
+
+    digitalWrite(kPinLed0, HIGH);
+    GGWave::send(kPinSpeaker, tx, GGWave::TX_ARDUINO_512_FASTEST);
+    digitalWrite(kPinLed0, LOW);
+
+    delay(250);
+
+    digitalWrite(kPinLed0, HIGH);
+    GGWave::send(kPinSpeaker, "This is GGWave!!", GGWave::TX_ARDUINO_512_FASTEST);
+    digitalWrite(kPinLed0, LOW);
+
+    delay(5000);
 }
